d99865d3b12f3bb954699072c3f22c77fe9f3799
legamre <legamres@gmail.com>
2024-08-15 21:20:56 +0200

    Create iris-classification.R

---
+++ iris-classification.R
@@ -0,0 +1,16 @@
+> library(datasets)
+> data("iris")
+> 
+> iris <- datasets::iris
+> View(iris)
+> View(iris)
+> iris$Petal.Length
+> iris$Species
+> Species <- iris$Species
+> Species
+> head(iris, 7)
+> tail(iris,7)
+> summary(iris)
+> summary(iris$Sepal.Length)
+> sum(is.na(iris))
+> skim(iris)

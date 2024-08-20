1295148d2952ab28d47bd9cde4ee06c1cc30522f
legamre <legamres@gmail.com>
2024-08-20 14:54:21 +0200

    Update iris.R

--- iris.R
+++ iris.R
@@ -1,18 +1,21 @@
-# Importing the Iris dataset
+# Loading necessary libraries
 library(datasets)
+library(skimr)
+library(caret)
+library(dplyr)
+
+# Importing the Iris dataset
 data("iris")
 iris <- datasets::iris
 
 # Data exploration
 View(iris)
-iris$Petal.Length
-iris$Species
-Species <- iris$Species
-Species
+str(iris)
+names(iris)
 head(iris, 7)
-tail(iris,7)
+tail(iris, 7)
 
-# Finding basic statistics
+# Basic statistics
 summary(iris)
 summary(iris$Sepal.Length)
 
@@ -20,78 +23,61 @@ summary(iris$Sepal.Length)
 sum(is.na(iris))
 
 # Expanding basic statistics
-library(skimr)
 skim(iris)
 
 # Expanding basic statistics after grouping data by species
 iris %>%
-dplyr::group_by(Species) %>%
-skim()
+  dplyr::group_by(Species) %>%
+  skimr::skim()
 
 # Panel plots
 plot(iris, col = "red")
 
 # Scatter plot
-plot(iris$Sepal.Width, iris$Sepal.Length, col = "red", xlab = "Sepal width", ylab = "Sepal length")
+plot(iris$Sepal.Width, iris$Sepal.Length, col = "red", 
+     xlab = "Sepal width", ylab = "Sepal length", 
+     main = "Sepal Width vs Length")
 
 # Histogram
-hist(iris$Sepal.Width, col = "red")  
+hist(iris$Sepal.Width, col = "red", main = "Histogram of Sepal Width")
 
 # Feature plots
-library(caret)
-featurePlot(x = iris[,1:4], 
+featurePlot(x = iris[, 1:4], 
             y = iris$Species, 
             plot = "box",
-            strip=strip.custom(par.strip.text=list(cex=.7)),
-            scales = list(x = list(relation="free"), 
-                          y = list(relation="free")))
-
+            strip = strip.custom(par.strip.text = list(cex = .7)),
+            scales = list(x = list(relation = "free"), 
+                          y = list(relation = "free")))
 
 # Machine learning in R
 set.seed(100)
 
 # Performs stratified random split of the data set
-TrainingIndex <- createDataPartition(iris$Species, p=0.8, list = FALSE)
+TrainingIndex <- createDataPartition(iris$Species, p = 0.8, list = FALSE)
 TrainingSet <- iris[TrainingIndex,] # Training Set
 TestingSet <- iris[-TrainingIndex,] # Test Set
 
-# Compare scatter plot of the 80 and 20 data subsets
-
 # SVM model (polynomial kernel)
-
-# Build Training model
+# Build Training model with cross-validation
 Model <- train(Species ~ ., data = TrainingSet,
                method = "svmPoly",
                na.action = na.omit,
-               preProcess=c("scale","center"),
-               trControl= trainControl(method="none"),
-               tuneGrid = data.frame(degree=1,scale=1,C=1))
+               preProcess = c("scale", "center"),
+               trControl = trainControl(method = "cv", number = 10),
+               tuneGrid = data.frame(degree = 1, scale = 1, C = 1))
 
-# Build CV model
-Model.cv <- train(Species ~ ., data = TrainingSet,
-                  method = "svmPoly",
-                  na.action = na.omit,
-                  preProcess=c("scale","center"),
-                  trControl= trainControl(method="cv", number=10),
-                  tuneGrid = data.frame(degree=1,scale=1,C=1))
+# Apply model for prediction on Training set
+Model.training.predictions <- predict(Model, TrainingSet)
 
-
-# Apply model for prediction
-# Apply model to make prediction on Training set
-Model.training <-predict(Model, TrainingSet)
- # Apply model to make prediction on Testing set
-Model.testing <-predict(Model, TestingSet)
-# Perform cross-validation
-Model.cv <-predict(Model.cv, TrainingSet) 
+# Apply model for prediction on Testing set
+Model.testing.predictions <- predict(Model, TestingSet)
 
 # Model performance (Displays confusion matrix and statistics)
-Model.training.confusion <-confusionMatrix(Model.training, TrainingSet$Species)
-Model.testing.confusion <-confusionMatrix(Model.testing, TestingSet$Species)
-Model.cv.confusion <-confusionMatrix(Model.cv, TrainingSet$Species)
+Model.training.confusion <- confusionMatrix(Model.training.predictions, TrainingSet$Species)
+Model.testing.confusion <- confusionMatrix(Model.testing.predictions, TestingSet$Species)
 
 print(Model.training.confusion)
 print(Model.testing.confusion)
-print(Model.cv.confusion)
 
 # Feature importance
 Importance <- varImp(Model)
